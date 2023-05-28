a833023cea6f8e74b53d5d2e1a72c2c0a99d76f8
Vladimir <iamvar@gmail.com>
2023-05-28 09:52:55 +0300

    Init (#1)
    * init

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# math
-Calculate math expression, using bc-power
+# Math
+
+Every developer knows: float is not the best choice for precise calculation.
+
+```bash
+php > var_dump(1.2 * 3);
+float(3.5999999999999996)
+```
+
+We have bcmath, you say.
+
+Yes, but calculating small expression, like `($a + $b * $c - $d) / $e` becomes as heavy as
+
+```php
+<?php
+
+$result = bcdiv(bcsub(bcadd($a, bcmul($b, $c)), $d), $e);
+```
+
+That is where this tool becomes useful:
+
+With registered helpers functions, you can just use
+
+```php
+<?php
+
+$result = calc("$a + $b * $c - $d");
+```
+
+That's it!
+
+All calculations behind the scene use bcmath functions.
+
+```bash
+php > var_dump(calc("1.2 * 3"));
+string("3.6")
+
+php > isTrue('1 < 2 < 3'); // true
+```
+
+## Helper Functions
+
+To register global helper functions, just update your composer.json with
+
+```
+"autoload": {
+    "files": [
+        "vendor/iamvar/math/src/helpers.php"
+    ]
+},
+```
+
+## Examples
+
+```php
+<?php
+
+$a = 0.1E1;
+$b = 2e-7;
+$c = 2;
+calc("$a + $b * $c"); // 1.0000004
+calc("1.69 ^ (1/2) + (0.1 - 0.25) * 2"); // 1
+
+isTrue("(1 + 0.2) * 3 == 3.6 + (1-9) * 0"); // true
+isTrue('1 < 2 < 3'); // true
+isTrue('1 < 2 > 3'); // false
+
+
+```
+
+## bcscale
+
+All calculations are performed with scale, taken by default from bcscale().   
+Do not forget to set required bcscale in your bootstrap file.
+
+```php
+<?php
+
+bcscale(25);
+```
+
+## Usage
+
+By default `calc` function cuts trailing zeroes,  
+e.g with scale = 4 `calc("0.1 + 0.9")` will return `"1"`
+
+If you want to save scale, add second parameter as false  
+`calc("0.1 + 0.9", false)` will return `"1.0000"`
+
+## License
+
+Math is licensed under the [MIT License](LICENSE).
---
+++ src/Math.php
@@ -0,0 +1,174 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Iamvar;
+
+class Math
+{
+    private const DEFAULT_PRECISION = 15;
+    private const NUMBER_REGEXP = '-?\d+(?:\.\d+)?';
+
+    private readonly int $scale;
+
+    public function __construct(?int $scale = null)
+    {
+        if ($scale === null) {
+            $scale = bcscale() === 0 ? self::DEFAULT_PRECISION : bcscale();
+        }
+
+        $this->scale = $scale;
+    }
+
+    /**
+     * Calculates expression with bcmath extension,
+     * e.g. "1 + 1.2 * 3" will return "4.6"
+     */
+    public function calc(string $expression, bool $cutTrailingZeroes = true): string
+    {
+        $expression = $this->prepare($expression);
+
+        while (preg_match('~\(.*\)~', $expression)) {
+            $expression = preg_replace_callback(
+                '~\(([^()]+)\)~',
+                fn(array $matches) => $this->calcExpressionWithoutBraces($matches[1]),
+                $expression
+            );
+        }
+
+        $result = $this->calcExpressionWithoutBraces($expression);
+
+        $this->checkIsNumber($result);
+
+        if ($cutTrailingZeroes) {
+            return $this->trimTrailingZeroes($result);
+        }
+
+        return $result;
+    }
+
+    private function calcExpressionWithoutBraces(string $expression): string
+    {
+        // first multiply, divide, power, mod. Then add, subtract
+        $numberRegex = self::NUMBER_REGEXP;
+        foreach (["[*/^%]", "[+-]"] as $operations) {
+            $regexp = "~($numberRegex)($operations)($numberRegex)~";
+            while (preg_match($regexp, $expression, $m)) {
+                $expression = preg_replace_callback(
+                    $regexp,
+                    [$this, 'bcOperation'],
+                    $expression
+                );
+            }
+        }
+
+        return $expression;
+    }
+
+    /**
+     * Checks if expression is true,
+     * e.g. "1.2 * 3 == 3.6" will return true
+     * as well as 1 < 2 < (1 - 3 + 5)
+     * Comparison is done with bccomp
+     */
+    public function isTrue(string $expression): bool
+    {
+        $expression = $this->prepare($expression);
+
+        $matches = preg_split('~(<|>|={1,3})~', $expression, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+        if (!isset($matches[2])) { //check we have right operand
+            throw new \ValueError('Bad expression');
+        }
+
+        $i = 1;
+        while (isset($matches[$i])) {
+            [$left, $operator, $right] = [$matches[$i - 1], $matches[$i], $matches[$i + 1]];
+            $compare = match ($operator) {
+                '>' => 1,
+                '<' => -1,
+                '=', '==', '===' => 0,
+            };
+
+            $left = $this->calc($left);
+            $right = $this->calc($right);
+            if (bccomp($left, $right, $this->scale) !== $compare) {
+                return false;
+            }
+
+            $i += 2;
+        }
+
+        return true;
+    }
+
+    private function bcOperation(array $matches): string
+    {
+        [$left, $right] = [$matches[1], $matches[3]];
+        return match ($matches[2]) {
+            '*' => bcmul($left, $right, $this->scale),
+            '/' => bcdiv($left, $right, $this->scale),
+            '%' => bcmod($left, $right, $this->scale),
+            '^' => $this->pow($left, $right),
+            '+' => bcadd($left, $right, $this->scale),
+            '-' => bcsub($left, $right, $this->scale),
+        };
+    }
+
+    private function prepare(string $expression): string
+    {
+        // remove all whitespaces
+        $expression = preg_replace('~\s+~', '', $expression);
+
+        //replace float numbers, like 2.1E-1 with 0.21
+        return preg_replace_callback(
+            '~\d+(?:\.\d+)?E[-+]?\d+~',
+            function (array $matches) {
+                [$num, $exponent] = explode('E', $matches[0]);
+
+                if ($exponent === '0') {
+                    return $num;
+                }
+
+                return bcmul($num, bcpow('10', $exponent, $this->scale), $this->scale);
+            },
+            $expression
+        );
+    }
+
+    private function checkIsNumber(string $subject): void
+    {
+        $numberRegExp = self::NUMBER_REGEXP;
+        if (!preg_match("~^$numberRegExp$~", $subject)) {
+            throw new \ValueError('Bad expression');
+        }
+    }
+
+    /**
+     * bcpow supports only integer $exponent o_O
+     * @link https://stackoverflow.com/questions/33486170/php-how-to-raise-number-to-tiny-fractional-exponent
+     */
+    public function pow(string $number, string $exponent): string
+    {
+        if (bccomp($number, '0', $this->scale) === 0) {
+            return '0';
+        }
+
+        $exponent = $this->trimTrailingZeroes($exponent);
+
+        if (ctype_digit($exponent)) {
+            return bcpow($number, $exponent, $this->scale);
+        }
+
+        return (string)((float)$number ** (float)$exponent);
+    }
+
+    private function trimTrailingZeroes(string $number): string
+    {
+        if (str_contains($number, '.')) {
+            return rtrim(rtrim($number, '0'), '.');
+        }
+
+        return $number;
+    }
+}
